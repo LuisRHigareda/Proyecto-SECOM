7d847cf1450b185a3f48e56f6fbd9b6650d967e6
SebasGlez03 <sebas.glez18040465@gmail.com>
2026-03-15 20:38:43 -0700

    Client - User (extension)
    Un cliente es un usuario, por lo que se hizo que el cliente extendiera del usuario.

--- SECOM_Project/SECOM_Domain/src/main/java/itson/secom_domain/Cliente.java
+++ SECOM_Project/SECOM_Domain/src/main/java/itson/secom_domain/Cliente.java
@@ -8,47 +8,74 @@ package itson.secom_domain;
  *
  * @author Serva
  */
-public class Cliente {
-    private int id;
+public class Cliente extends Usuario{
+
+    private int idCliente;
     private String nombreCompleto;
     private String telefono;
     private String correoElectronico;
     private String direccion;
- 
-   public Cliente() {}
- 
-   public Cliente(int id, String nombreCompleto, String telefono,
-                   String correoElectronico, String direccion) {
-        this.id                = id;
-        this.nombreCompleto    = nombreCompleto;
-        this.telefono          = telefono;
-        this.correoElectronico = correoElectronico;
-        this.direccion         = direccion;
+
+    public Cliente() {
     }
- 
-  public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
- 
-    public String getNombreCompleto() { return nombreCompleto; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
- 
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
- 
-    public String getCorreoElectronico() { return correoElectronico; }
-    public void setCorreoElectronico(String correoElectronico) { this.correoElectronico = correoElectronico; }
- 
-    public String getDireccion() { return direccion; }
-    public void setDireccion(String direccion) { this.direccion = direccion; }
- 
-     @Override
+
+    public Cliente(int id, String nombreCompleto, String telefono,
+            String correoElectronico, String direccion) {
+        this.idCliente = id;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.direccion = direccion;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int id) {
+        this.idCliente = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
     public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nombreCompleto='" + nombreCompleto + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", correoElectronico='" + correoElectronico + '\'' +
-                ", direccion='" + direccion + '\'' +
-                '}';
+        return "Cliente{"
+                + "id=" + idCliente
+                + ", nombreCompleto='" + nombreCompleto + '\''
+                + ", telefono='" + telefono + '\''
+                + ", correoElectronico='" + correoElectronico + '\''
+                + ", direccion='" + direccion + '\''
+                + '}';
     }
 }
